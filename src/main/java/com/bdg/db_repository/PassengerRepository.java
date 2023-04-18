389c41013332faac9a478ec253de95dfd9ae0887
Arman Hambardzumyan <arman.hambardzumyan.2232@gmail.com>
2023-04-19 01:18:29 +0400

    Updated PassengerService.java and PassengerRepository.java files, added implementations of some methods in PassengerService.java file

--- src/main/java/com/bdg/db_repository/PassengerRepository.java
+++ src/main/java/com/bdg/db_repository/PassengerRepository.java
@@ -1,4 +1,28 @@
 package com.bdg.db_repository;
 
+import com.bdg.model.Passenger;
+import com.bdg.model.Trip;
+
+import java.util.List;
+import java.util.Set;
+
 public interface PassengerRepository {
+
+    Passenger getBy(int id);
+
+    Set<Passenger> getAll();
+
+    Set<Passenger> get(int offset, int perPage, String sort);
+
+    Passenger save(Passenger passenger);
+
+    Passenger updateBy(int id, Passenger passenger);
+
+    void deleteBy(int id);
+
+    List<Passenger> getPassengersOfTrip(int tripNumber);
+
+    void registerTrip(Trip trip, Passenger passenger);
+
+    void cancelTrip(int passengerId, int tripNumber);
 }
